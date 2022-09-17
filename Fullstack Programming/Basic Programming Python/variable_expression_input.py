933f697b7760192179c46c4b69f1cbd38d4fae2f
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-17 16:51:10 +0700

    Add basic programming python

---
+++ Fullstack Programming/Basic Programming Python/variable_expression_input.py
@@ -0,0 +1,9 @@
+# Expression is data value or set operation to compute a value
+print(1 + 4 * 3)  # value of operation
+
+# Variable is a named piece of memory that can store a value
+# name = value
+number_one = 5
+
+# print() is function to produce text output on the console
+print('Hello Arief')
