261feccfe3a0c84407bae171405882bf1c526dda
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-17 16:58:11 +0700

    add for loop

---
+++ Fullstack Programming/Basic Programming Python/for_loop.py
@@ -0,0 +1,9 @@
+# for loop is repeated a task of statement over a group of values
+# for variableName in groupOfValues
+# statement
+
+for x in range(1, 6):
+    print(x, "squared is", x * x)
+
+for y in range(5, 0):  # won't work
+    print(y)
