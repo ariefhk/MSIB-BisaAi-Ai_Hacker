a9160142be9b14bcff7615b7a2d96357a2241b88
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-17 17:08:49 +0700

    add countable loop

--- Fullstack Programming/Basic Programming Python/for_loop.py
+++ Fullstack Programming/Basic Programming Python/for_loop.py
@@ -2,8 +2,15 @@
 # for variableName in groupOfValues
 # statement
 
+print('Basic Loop: ')
 for x in range(1, 6):
     print(x, "squared is", x * x)
 
 for y in range(5, 0):  # won't work
     print(y)
+
+print('Cumulative Loop: ')
+sum = 0
+for i in range(1, 11):
+    sum = sum + (i * i)
+print('sum of first 10 is ', sum)
